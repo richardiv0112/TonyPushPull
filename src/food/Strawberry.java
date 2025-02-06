ca2c9cfb41ad8908de0cb9c007e5a0d5932b46c8
Richard Bridgeman <richardivjob@gmail.com>
2025-02-06 16:13:22 -0600

    another one

--- src/food/Strawberry.java
+++ src/food/Strawberry.java
@@ -4,6 +4,9 @@ public class Strawberry {
 
 	public static void main(String[] args) {
 		System.out.println("I eat strawberries");
+		System.out.println("I eat strawberries");
+		System.out.println("I eat strawberries");
+		System.out.println("I eat strawberries");
 
 	}
 
